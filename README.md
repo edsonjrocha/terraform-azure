ed1c727b61be6a0a285ad9552a55fdcee2a8b92a
Edson Rocha <edson.ejr@hotmail.com>
2021-04-27 20:32:55 -0300

    Subindo arquivos

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# terraform-azure
-ESD21 - Infrastructure and Cloud Computing - Exercício Aula 02: Criar uma máquina virtual na Azure utilizando Terraform com MySQL 
+# terraform-azure  
+
+## IMPACTA MBA ENGENHARIA DE SOFTWARE  
+## ESD21 - Infrastructure and Cloud Computing 
+---------------------------------------------------------------------------------------------------------------
+
+## Exercício Aula 02
+
+## Aluno: Edson João da Rocha
+
+## Objetivo: Criar uma máquina virtual com MySQL na Azure utilizando Terraform
+
+Para subir a Máquina Virtual na azure:  
+    1 - Criar uma pasta na sua máquina local.  
+    2 - Clonar o repositório: "git clone https://github.com/edsonjrocha/terraform-azure.git".  
+    3 - Logar com a sua conta Azure* via comando: "az login".  
+    4 - Subir o ambiente utilizando terraform* via comando: "terraform apply".  
+
+*Obrigatório ter o terraforme e o az-cli instalado na sua máquina.  
+
+Para acessar ao banco de dados:  
+Client: MySQL Workbench 6.3 (ou similar)  
+IP: [IP_PUBLICO_NO_SEU_PORTAL_AZURE] Porta: 3306  
+Usuário: teste  
+Senha: teste
---
+++ main.tf
@@ -0,0 +1,171 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+    skip_provider_registration = false
+    features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "tf-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "acceptanceTestPublicIp1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+}
+
+data "azurerm_public_ip" "ip_public_sql" {
+  name                = azurerm_public_ip.example.name
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "mysql"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3306"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "tf-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id 
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "tf-vm"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+}
+
+output "public_ip_address_mysql" {
+  value = azurerm_public_ip.example.ip_address
+}
+
+resource "time_sleep" "wait_30_seconds_db" {
+  depends_on = [azurerm_virtual_machine.main]
+  create_duration = "30s"
+}
+
+resource "null_resource" "upload_db" {
+    provisioner "file" {
+        connection {
+            type = "ssh"
+            user = "testadmin"
+            password = "Password1234!"
+            host = data.azurerm_public_ip.ip_public_sql.ip_address
+        }
+        source = "config"
+        destination = "/home/testadmin"
+    }
+
+    depends_on = [ time_sleep.wait_30_seconds_db ]
+}
+
+resource "null_resource" "deploy_db" {
+    triggers = {
+        order = null_resource.upload_db.id
+    }
+    provisioner "remote-exec" {
+        connection {
+            type = "ssh"
+            user = "testadmin"
+            password = "Password1234!"
+            host = data.azurerm_public_ip.ip_public_sql.ip_address
+        }
+        inline = [
+            "sudo apt-get update",
+            "sudo apt-get install -y mysql-server-5.7",
+            "sudo mysql < /home/testadmin/config/user.sql",
+            "sudo cp -f /home/testadmin/config/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf",
+            "sudo service mysql restart",
+            "sleep 20",
+        ]
+    }
+}
